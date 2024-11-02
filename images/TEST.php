4a2c91c35b4d724eb224cc08b9f4115a5866dc64
Jatinder Kumar Sharma <67006598+jatindervision@users.noreply.github.com>
2024-11-02 19:33:21 +0530

    Add files via upload

---
+++ images/TEST.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>ICL COMPUTER EDUCATION</title>
+<style type="text/css">
+body { max-width: 1366px; min-width: 650px; margin: 0 auto; border: none; padding: 0; }
+</style>
+<canvas id="canvas_tt5f548793c3ba8" width="125" height="125"></canvas>
+<a href="//24timezones.com/current_time/india_new_delhi_clock.php" style="text-decoration: none" class="clock24" id="tz24-1599375251-cc13072-eyJzaXplIjoiMTI1IiwiYmdjb2xvciI6IkZGRkZGRiIsImxhbmciOiJlbiIsInR5cGUiOiJhIiwiY2FudmFzX2lkIjoiY2FudmFzX3R0NWY1NDg3OTNjM2JhOCJ9" title="actual time New Delhi" target="_blank" rel="nofollow"></a>
+          <script type="text/javascript" src="//w.24timezones.com/l.js" async></script>
+</head>
+
+<body>
+<center>
+COMPUTER EDUCATION <h1>jghjghjghjhgjhgjgh   jghjghjkhfgtryrtytryrt  yrtyrttyrtyryryrtyrtyrt  ygjghj   ghjghjgh   jghjghjgh  jghjgh ghj ghj ghj ghj ghj ghj jjjjxxxxxx</h1>
+</center>
+</body>
+</html>
